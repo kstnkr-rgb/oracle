6313ca88a71834c8181080de088b9815653f9215
kstnkr-rgb <kstnkr@gmail.com>
2026-03-12 10:24:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
-const ASTRO_KEY = process.env.ASTRO_KEY || 'ask_bf45faa53077f486c9de8b404b525e8a7fcf48f7fa71792376f3c5432605e0';
+const ASTRO_KEY = process.env.ASTRO_KEY || 'ask_bf45faa53077f486c9de8b404b525e8a7fcf48f7fa71792376f3c5432605e045';
 const ASTRO_HOST = 'api.astrology-api.io';
 
 const server = http.createServer((req, res) => {
